0114b67d75f774d53164ce9c66c95f4b4609402a
MattDotExe <Mbrittain7134@gmail.com>
2014-12-09 22:41:47 -0600

    Create items.py

---
+++ items.py
@@ -0,0 +1,15 @@
+class Item():
+    """The base class for all items"""
+    def __init__(self, name, description, value):
+        self.name = name
+        self.description = description
+        self.value = value
+ 
+    def __str__(self):
+        return "{}\n=====\n{}\nValue: {}\n".format(self.name, self.description, self.value)
+class Gold(Item):
+    def __init__(self, amt):
+        self.amt = amt
+        super().__init__(name="Silver",
+                         description="A beautifully decorated round coin with {} stamped on the front. It is said that these coins were only found on bodies lurking inside the ruins. Most people disagree with this theory however because not one person has ever returned from The Ruins alive. .".format(str(self.amt)),
+                         value=self.amt) 		
