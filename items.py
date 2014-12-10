a588ae5aff0fe8b44bdbdcba19534459e477a600
MattDotExe <Mbrittain7134@gmail.com>
2014-12-10 13:28:53 -0600

    Update items.py

--- items.py
+++ items.py
@@ -7,9 +7,40 @@ class Item():
  
     def __str__(self):
         return "{}\n=====\n{}\nValue: {}\n".format(self.name, self.description, self.value)
-class Gold(Item):
+class Silver(Item):
     def __init__(self, amt):
         self.amt = amt
         super().__init__(name="Silver",
                          description="A beautifully decorated round coin with {} stamped on the front. It is said that these coins were only found on bodies lurking inside the ruins. Most people disagree with this theory however because not one person has ever returned from The Ruins alive. .".format(str(self.amt)),
-                         value=self.amt) 		
+                         value=self.amt)
+                         class Weapon(Item):
+    def __init__(self, name, description, value, damage):
+        self.damage = damage
+        super().__init__(name, description, value)
+ 
+    def __str__(self):
+        return "{}\n=====\n{}\nValue: {}\nDamage: {}".format(self.name, self.description, self.value, self.damage)
+ 
+ 
+class Rock(Weapon):
+    def __init__(self):
+        super().__init__(name="Rock",
+                         description="A fist-sized rock, suitable for bludgeoning.",
+                         value=0,
+                         damage=5)
+ 
+ 
+class Wooden Club(Weapon):
+    def __init__(self):
+        super().__init__(name="Wooden Club",
+                         description="A Large Wooden Club that seems to have a dried blood covered everywhere. I wonder what happened..",
+                         value=10,
+                         damage=10)
+                         
+class Brimstone(Weapon):
+    def __init__(self):
+        super().__init__(name="Brimstone",
+                         description="A stone completely identical to the Rock, although strangely enough you feel as though it has a skin-like texture to it. It seems to often pus out blood in a straight line at random",
+                         value=0,
+                         damage=5)
+                         
