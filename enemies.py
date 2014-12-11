8edc47acfb1570b02e5138999e031ff04d47e9da
MattDotExe <Mbrittain7134@gmail.com>
2014-12-11 00:27:26 -0600

    enemies.py

---
+++ enemies.py
@@ -0,0 +1,16 @@
+class Enemy:
+    def __init__(self, name, hp, damage):
+        self.name = name
+        self.hp = hp
+        self.damage = damage
+ 
+    def is_alive(self):
+        return self.hp > 0                         
+class Tiny Spider(Enemy):
+    def __init__(self):
+        super().__init__(name="Tiny Spider", hp=5, damage=2)
+ 
+ 
+class Skeleton(Enemy):
+    def __init__(self):
+        super().__init__(name="Skeleton", hp=15, damage=10)
