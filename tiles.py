f276fbcd4baaac905099ca1963b8fcae41d5af71
MattDotExe <Mbrittain7134@gmail.com>
2014-12-11 00:43:09 -0600

    Create tiles.py

---
+++ tiles.py
@@ -0,0 +1,22 @@
+from The-Ruins-Text-Adventure import items, enemies
+ 
+class MapTile:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+def intro_text(self):
+    raise NotImplementedError()
+ 
+def modify_player(self, player):
+    raise NotImplementedError()
+class StartingRoom(MapTile):
+    def intro_text(self):
+        return """
+        You awake with only the simple idea that you were sent here by your village. You remember being awaked by the priest early yesterday morning with him telling you that you would be taken to The Ruins just outside the village
+        as you hear the grinding shut to the door behind you, everything becomes dark and you fiddle to find you torch. As the blazing warmth of the torch engulfs your body, you see that there are for different pathways for you to walk down.
+        
+        """
+ 
+    def modify_player(self, player):
+        #Room has no action on player
+        pass    
